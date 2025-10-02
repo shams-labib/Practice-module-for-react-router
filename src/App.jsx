b67b6c2416bb1376c15a420b857cbe025013528e
shams <shamsallabib@gmail.com>
2025-10-03 01:37:00 +0600

    added mal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import './App.css'
 import Nvaber from './Components/Nvaber'
-import Fisrt from './Pages/Fisrt'
 import Home from './Components/Home'
 import { Outlet } from 'react-router'
 
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            <h2 className='text-3xl'> Contact page</h2>
+        <div className='flex flex-col items-center justify-center mt-5 '>
+            <h2 className='text-3xl'> Contact Me</h2>
+
+            <div className='w-1/2 h-[30vh] border-1 border-gray-300  mt-5 flex flex-col justify-center items-center shadow-xl rounded-2xl p-5 space-y-2'>
+                 <h1 className='text-xl font-bold '>Name : Kodbel</h1>
+                 <p>Email : kodbelbaba@gmail.com </p>
+                 <p>Phone : 017000000</p>
+            </div>
         </div>
     );
 };
